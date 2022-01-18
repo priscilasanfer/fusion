c54814ce8fdcd191c14e5d22d13bf4923cfc1f19
Priscila Ferreira <priscilasanfer@gmail.com>
2022-01-18 12:36:08 -0300

    mudandno o debug pra False

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g8%)8=fw3soxm@k%09w2ch2wuosricm&lxo5d%+r#nnf==7%)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
